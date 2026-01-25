cb3e9f407589d552e3e3951991f2dfda4b66882e
Aleksey <agp251093@gmail.com>
2026-01-25 14:54:18 +0300

    shell

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Категория", unique=True)
-    description = models.TextField(verbose_name="Описание")
+    description = models.TextField(verbose_name="Описание", blank=True)
 
     def __str__(self):
         return self.name
