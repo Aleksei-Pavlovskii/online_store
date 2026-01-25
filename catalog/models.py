f1fd57575f65ea94a22fedc4675b4603103aeb75
Aleksey <agp251093@gmail.com>
2026-01-25 13:41:01 +0300

    Созданы модели продукта и категории

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "наименование"
+        verbose_name_plural = "наименования"
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="photos/", blank=True, null=True, verbose_name="Фото"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name="Категория",
+        related_name="products",
+        null=True,
+        blank=True,
+    )
+    price = models.FloatField(verbose_name="Цена за покупку")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "наименование"
+        verbose_name_plural = "наименования"
+        ordering = ["name"]
