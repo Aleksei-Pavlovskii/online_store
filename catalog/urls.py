03ef7dadea53d065e5b4004c3b58286089894dae
Aleksey <agp251093@gmail.com>
2026-02-19 18:48:51 +0300

    hw_26.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,10 +6,13 @@ app_name = "catalog"
 urlpatterns = [
     path("home/", views.HomeView.as_view(), name="home"),
     path("contacts/", views.ContactTemplateView.as_view(), name="contacts"),
-    path("", views.ProductListView.as_view(), name="products_list"),
+    path("", views.ProductListView.as_view(), name="product_list"),
     path(
         "product_details/<int:pk>/",
         views.ProductDetailView.as_view(),
         name="product_details",
     ),
+    path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('producct/update/<int:pk>/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
