a787178348956b30bfd593a9123374539ceebfa8
Aleksey <agp251093@gmail.com>
2026-03-07 13:56:34 +0300

    Реализована функция аутентификации пользователя

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('email', 'phone', 'country', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('is_active', 'is_staff', 'country', 'date_joined')
+    search_fields = ('email', 'phone', 'country')
+    ordering = ('email',)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,28 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email должен быть указан')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True')
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     username = None
@@ -27,6 +49,9 @@ class User(AbstractUser):
         null=True,
         help_text="Введите страну",
     )
+    token = models.CharField(max_length=100, verbose_name="Токен", blank=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -37,3 +62,6 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+
